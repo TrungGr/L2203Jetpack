7314c67c0a2f878ffa5d8b3e756815763e6c2703
Trung Doan <doanptand@gmail.com>
2022-09-14 20:33:53 +0700

    Implement UI with MVVM

--- app/src/main/java/com/ddona/l2003jetpack/MainActivity.kt
+++ app/src/main/java/com/ddona/l2003jetpack/MainActivity.kt
@@ -2,24 +2,36 @@ package com.ddona.l2003jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.ddona.l2003jetpack.databinding.ActivityMainBinding
+import com.ddona.l2003jetpack.vm.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var number: Int = 0
+    private lateinit var viewModel: MainViewModel
+
+//    var number: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Cách khởi tạo
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.btnUp.setOnClickListener {
-            upNumber()
+            viewModel.updateNumber()//2
+//            upNumber()
+        }
+
+        viewModel.number.observe(this) {
+            //lắng nghe
+            binding.tvNumber.text = it.toString()//2
         }
     }
 
-    private fun upNumber() {
-        number = number.plus(1)
-        binding.tvNumber.text = number.toString()
-    }
+//    private fun upNumber() {
+//        number = number.plus(1)
+//        binding.tvNumber.text = number.toString()
+//    }
 }
--- app/src/main/java/com/ddona/l2003jetpack/vm/MainViewModel.kt
+++ app/src/main/java/com/ddona/l2003jetpack/vm/MainViewModel.kt
@@ -1,6 +1,13 @@
 package com.ddona.l2003jetpack.vm
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
+
+    var number = MutableLiveData<Int>(0) //observer pattern //2
+
+    fun updateNumber() {
+        number.value = number.value!!.plus(1)//2
+    }
 }
