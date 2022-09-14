fdd4dffe67debcbcf0f7cd35f4f78b0fb8fe843e
Trung Doan <doanptand@gmail.com>
2022-09-14 20:22:42 +0700

    Implement UI without MVVM

--- app/src/main/java/com/ddona/l2003jetpack/MainActivity.kt
+++ app/src/main/java/com/ddona/l2003jetpack/MainActivity.kt
@@ -2,10 +2,24 @@ package com.ddona.l2003jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ddona.l2003jetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var number: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnUp.setOnClickListener {
+            upNumber()
+        }
+    }
+
+    private fun upNumber() {
+        number = number.plus(1)
+        binding.tvNumber.text = number.toString()
     }
 }
---
+++ app/src/main/java/com/ddona/l2003jetpack/vm/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.ddona.l2003jetpack.vm
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
